a2229d44d5f357b928fb7c64ed53643ac957c52d
Asif Mohammad Mollah <asif.mollah@gmail.com>
2019-11-20 16:45:40 +0530

    data binding implemented

--- app/src/main/java/in/mrasif/dev/mvvmdemo/activities/MainActivity.java
+++ app/src/main/java/in/mrasif/dev/mvvmdemo/activities/MainActivity.java
@@ -2,40 +2,36 @@ package in.mrasif.dev.mvvmdemo.activities;
 
 import android.app.ProgressDialog;
 import android.arch.lifecycle.ViewModelProviders;
-import android.support.design.widget.FloatingActionButton;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 
 import in.mrasif.dev.mvvmdemo.R;
 import in.mrasif.dev.mvvmdemo.adapters.NoteAdapter;
-import in.mrasif.dev.mvvmdemo.models.Note;
+import in.mrasif.dev.mvvmdemo.databinding.ActivityMainBinding;
+import in.mrasif.dev.mvvmdemo.dialogs.AddNoteDialog;
 import in.mrasif.dev.mvvmdemo.viewmodels.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView rvList;
-    private FloatingActionButton fabAdd;
+
     private NoteAdapter adapter;
     private ProgressDialog pdIsAdding;
+    private AddNoteDialog addNoteDialog;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        rvList=findViewById(R.id.rvNotes);
-        fabAdd=findViewById(R.id.fabAdd);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
         adapter=new NoteAdapter(this);
-        rvList.setLayoutManager(new LinearLayoutManager(this));
-        rvList.setAdapter(adapter);
+        binding.setAdapter(adapter);
 
         MainViewModel model= ViewModelProviders.of(this).get(MainViewModel.class);
         model.getNotes().observe(this,notes -> {
             adapter.update(notes);
         });
-
 
         pdIsAdding=new ProgressDialog(this);
         pdIsAdding.setMessage("Note adding...");
@@ -48,11 +44,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        fabAdd.setOnClickListener(view -> {
-            Note note=new Note();
-            note.setTitle("Any title");
-            note.setDescription("Any description");
-            model.addNewValue(note);
+        binding.fabAdd.setOnClickListener(view -> {
+            addNoteDialog=new AddNoteDialog(this,model);
+            addNoteDialog.show();
         });
     }
 }
